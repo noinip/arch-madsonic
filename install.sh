79577100f9346d83b54efac34186de35f69efa37
binhex <megalith01@gmail.com>
2015-08-14 09:19:49 +0100

    updated to latest stable, 5240

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ mkdir -p /opt/madsonic/media
 mkdir -p /opt/madsonic/transcode
 
 # download madsonic standalone
-curl -o /opt/madsonic/madsonic.zip -L http://www.madsonic.org/download/5.1/20141017_madsonic-5.1.5200-standalone.zip
+curl -o /opt/madsonic/madsonic.zip -L http://www.madsonic.org/download/5.1/20150506_madsonic-5.1.5240-standalone.zip
 
 # download madsonic transcode
 curl -o /opt/madsonic/transcode/transcode.zip -L http://www.madsonic.org/download/transcode/20141017_madsonic-transcode_latest_x64.zip
